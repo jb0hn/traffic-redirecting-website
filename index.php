77f67d2b997d3cb2c443593dd5b7b24e732209a3
Józef Bongowski <j.bongowski@protonmail.com>
2018-09-22 16:45:32 +0200

    Change redirect method in order to set up Google Analytics tracking

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <?php require 'put_stats.php'; ?>
-<?php
-  header('Location: '.$newURL);
-  die();
-?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=GA_TRACKING_ID"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'GA_TRACKING_ID');
+    </script>
+
     <meta charset="utf-8">
     <title>Badanie magisterskie - przekierowanie</title>
+    <script type="text/javascript">
+      window.location = '<?php echo $newURL; ?>';
+    </script>
   </head>
   <body>
   </body>
